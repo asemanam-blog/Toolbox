3e8baf072454bf665151bcfa3741f2913109ee81
Mohammadreza Moghimi <mimr.moghimi@gmail.com>
2021-03-03 05:50:57 +0330

    add some comments

--- in-post-linker.js
+++ in-post-linker.js
@@ -1,9 +1,18 @@
 <script>
-  let headers = document.querySelectorAll('h3, h4, h5');
+  // Capture All hn tags of post content
+  let headers = document.querySelectorAll('h3, h4, h5, h6');
+
+  // Loop through all hn tags and add id attribute
   for (let i = 0; i < headers.length; i++) {
     headers[i].setAttribute('id', 'title-' + i);
   }
-  let headerss = document.querySelectorAll('h3, h4, h5');
+
+
+  // Capture hn's after the loop again
+  let headerss = document.querySelectorAll('h3, h4, h5, h6');
+
+  // Loop through captured tags and Create an a element
+  // then replace them with it's inner html
   for (let j = 0; j < headers.length; j++) {
     let isame = headerss[j].getAttribute('id');
     let kisame = document.createElement('a');
